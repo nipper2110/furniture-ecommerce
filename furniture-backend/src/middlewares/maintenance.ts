14b08c558125f531ab6fa48ce6400ee5cdd1ef1f
Chan Nyein Moe <channyeinmoe2121@gmail.com>
2026-06-30 02:00:24 +0700

    Maintenance mode for auth

---
+++ furniture-backend/src/middlewares/maintenance.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import { getSettingStatus } from "../services/settingService";
+import { errorCode } from "../../config/errorCode";
+import { createError } from "../utils/error";
+
+const whiteLists = ["127.0.0.1"];
+
+export const maintenance = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const ip: any = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  if (whiteLists.includes(ip)) {
+    console.log(`Allow IP: ${ip}`);
+    next();
+  } else {
+    console.log(`Not Allow IP: ${ip}`);
+    const setting = await getSettingStatus("maintenance");
+    if (setting?.value === "true") {
+      return next(
+        createError(
+          "The server is currently under maintenance. Please try again later.",
+          503,
+          errorCode.maintenance,
+        ),
+      );
+    }
+
+    next();
+  }
+};
--- furniture-backend/src/routes/v1/index.ts
+++ furniture-backend/src/routes/v1/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import { auth } from "../../middlewares/auth";
 import { authorise } from "../../middlewares/authorise";
+import { maintenance } from "../../middlewares/maintenance";
 
 import healthRoutes from "./health";
 import authRoutes from "./auth";
@@ -12,7 +13,12 @@ const router = express.Router();
 
 router.use("/api/v1", healthRoutes);
 router.use("/api/v1", authRoutes);
-router.use("/api/v1/user", userRoutes);
+router.use("/api/v1/user", maintenance, userRoutes);
 router.use("/api/v1/admins", auth, authorise(true, "ADMIN"), adminRoutes);
+
+// router.use("/api/v1", healthRoutes);
+// router.use("/api/v1", maintenance, authRoutes);
+// router.use("/api/v1/user", maintenance, userRoutes);
+// router.use("/api/v1/admins", auth, authorise(true, "ADMIN"), adminRoutes);
 
 export default router;
