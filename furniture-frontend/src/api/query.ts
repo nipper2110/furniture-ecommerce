28a8dd16173012d0ce6d2b26fa3d6ece25926a2a
Chan Nyein Moe <channyeinmoe2121@gmail.com>
2026-07-27 19:43:44 +0700

    Add api for categories & types

--- furniture-frontend/src/api/query.ts
+++ furniture-frontend/src/api/query.ts
@@ -62,3 +62,11 @@ export const onePostQuery = (id: number) => ({
   queryKey: ["posts", "detail", id],
   queryFn: () => fetchOnePost(id),
 });
+
+const fetchCategoryType = async () =>
+  api.get("users/filter-type").then((res) => res.data);
+
+export const categoryTypeQuery = () => ({
+  queryKey: ["category", "type"],
+  queryFn: fetchCategoryType,
+});
